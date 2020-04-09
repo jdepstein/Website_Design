99652ea39a171b017d796f9b4f5382398b4aa5bf
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-04-09 13:00:20 -0500

    commas

--- populate.js
+++ populate.js
@@ -8,15 +8,15 @@ connect();
 
 
 const icecreams = [
-  new IceCream({Name: "Skunks Treat", flavor:["Vanilla"], fillings:["Oreo Cookie bits"], description:"Creamy Vanilla Ice Cream with Home Made Oreo Cookie Bits", Price:[("Pint", 10)], Calories: 1000, Locations:["Chocolate Chip City"]})
-  new IceCream({Name: "Mocha-licious", flavor:["Coffee"], fillings:[""], description:"No morning coffe? Mocha-licious will wake you up!", Price:[("Pint", 10)], Calories: 9000, Locations:[""]})
-  new IceCream({Name: "Choc It Out", flavor:["Chocolate"], fillings:["Cookie dough, Chocolate chips"], description:"Choc It Out with some friends over our delicious dough!", Price:[("Pint", 10)], Calories: 1100, Locations:[""]})
-  new IceCream({Name: "Vanilla Ice", flavor:["Vanilla"], fillings:[""], description:"The best Vanilla Ice-cream you'll ever taste!", Price:[("Pint", 10)], Calories: 700, Locations:[""]})
-  new IceCream({Name: "Burst Your Bubble", flavor:["Bubble Gum"], fillings:[""], description:"Try not Burst Your Bubble!", Price:[("Pint", 10)], Calories: 1000, Locations:[""]})
-  new IceCream({Name: "Fairy Floss", flavor:["Cotton Candy"], fillings:["Sprinkles"], description:"Your favorite county fair treat now available 24/7", Price:[("Pint", 10)], Calories: 1000, Locations:[""]})
-  new IceCream({Name: "sPEACHless", flavor:["Peach"], fillings:[""], description:"Your favorite fruit now in frozen form will leave you sPEACHless", Price:[("Pint", 10)], Calories: 800, Locations:[""]})
-  new IceCream({Name: "When It Rains It S'mores", flavor:["Vanilla"], fillings:["Graham crackers, Marshmallow fluff, Chocolate fugde"], description:"Delicious campfire smore's brought to you without the fire", Price:[("Pint", 10)], Calories: 1500, Locations:[""]})
-  new IceCream({Name: "Won't Fudge", flavor:["Chocolate"], fillings:["Chocolate fudge"], description:"This chocolate overload Won't Fudge your chocolate cravings", Price:[("Pint", 10)], Calories: 1100, Locations:[""]})
+  new IceCream({Name: "Skunks Treat", flavor:["Vanilla"], fillings:["Oreo Cookie bits"], description:"Creamy Vanilla Ice Cream with Home Made Oreo Cookie Bits", Price:[("Pint", 10)], Calories: 1000, Locations:["Chocolate Chip City"]}),
+  new IceCream({Name: "Mocha-licious", flavor:["Coffee"], fillings:[""], description:"No morning coffe? Mocha-licious will wake you up!", Price:[("Pint", 10)], Calories: 9000, Locations:[""]}),
+  new IceCream({Name: "Choc It Out", flavor:["Chocolate"], fillings:["Cookie dough, Chocolate chips"], description:"Choc It Out with some friends over our delicious dough!", Price:[("Pint", 10)], Calories: 1100, Locations:[""]}),
+  new IceCream({Name: "Vanilla Ice", flavor:["Vanilla"], fillings:[""], description:"The best Vanilla Ice-cream you'll ever taste!", Price:[("Pint", 10)], Calories: 700, Locations:[""]}),
+  new IceCream({Name: "Burst Your Bubble", flavor:["Bubble Gum"], fillings:[""], description:"Try not Burst Your Bubble!", Price:[("Pint", 10)], Calories: 1000, Locations:[""]}),
+  new IceCream({Name: "Fairy Floss", flavor:["Cotton Candy"], fillings:["Sprinkles"], description:"Your favorite county fair treat now available 24/7", Price:[("Pint", 10)], Calories: 1000, Locations:[""]}),
+  new IceCream({Name: "sPEACHless", flavor:["Peach"], fillings:[""], description:"Your favorite fruit now in frozen form will leave you sPEACHless", Price:[("Pint", 10)], Calories: 800, Locations:[""]}),
+  new IceCream({Name: "When It Rains It S'mores", flavor:["Vanilla"], fillings:["Graham crackers, Marshmallow fluff, Chocolate fugde"], description:"Delicious campfire smore's brought to you without the fire", Price:[("Pint", 10)], Calories: 1500, Locations:[""]}),
+  new IceCream({Name: "Won't Fudge", flavor:["Chocolate"], fillings:["Chocolate fudge"], description:"This chocolate overload Won't Fudge your chocolate cravings", Price:[("Pint", 10)], Calories: 1100, Locations:[""]}),
   new IceCream({Name: "Believe It Or Nut", flavor:["Peanut Butter"], fillings:["Peanut chunks, Chocolate fugde"], description:"Believe It or Nut it's a Chuncky Reese's", Price:[("Pint", 10)], Calories: 1200, Locations:[""]})
 
 
@@ -24,9 +24,9 @@ const icecreams = [
 
 
 const stores = [
-  new Store({_id: "Sundae_Fundae", City: "Chocolate_Chip", State: "NY", Street: "Vanilla Ave",Building_Number: 7, icecream:["Cookies & Cream"] })
-  new Store({_id: "Sundae_Fundae", City: "Milkshake", State: "IL", Street: "Rocky Road",Building_Number: 216, icecream:["Cookies & Cream"] })
-  new Store({_id: "Sundae_Fundae", City: "Cherry", State: "NH", Street: "Dairy Ave",Building_Number: 305, icecream:["Cookies & Cream"] })
+  new Store({_id: "Sundae_Fundae", City: "Chocolate_Chip", State: "NY", Street: "Vanilla Ave",Building_Number: 7, icecream:["Cookies & Cream, Won't Fudge"] }),
+  new Store({_id: "Sundae_Fundae", City: "Milkshake", State: "IL", Street: "Rocky Road",Building_Number: 216, icecream:["Cookies & Cream"] }),
+  new Store({_id: "Sundae_Fundae", City: "Cherry", State: "NH", Street: "Dairy Ave",Building_Number: 305, icecream:["Cookies & Cream"] }),
   new Store({_id: "Sundae_Fundae", City: "Sprinkles", State: "PA", Street: "Strawberry Lane",Building_Number: 5, icecream:["Cookies & Cream"] })
 ];
 
