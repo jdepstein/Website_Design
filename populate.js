114ce4f3bd3394737f6ff081c8fc1205956d02e6
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-04-09 13:02:00 -0500

    id names

--- populate.js
+++ populate.js
@@ -24,10 +24,10 @@ const icecreams = [
 
 
 const stores = [
-  new Store({_id: "Sundae_Fundae", City: "Chocolate_Chip", State: "NY", Street: "Vanilla Ave",Building_Number: 7, icecream:["Cookies & Cream, Won't Fudge"] }),
+  new Store({_id: "ice_cream", City: "Chocolate_Chip", State: "NY", Street: "Vanilla Ave",Building_Number: 7, icecream:["Cookies & Cream, Won't Fudge"] }),
   new Store({_id: "Sundae_Fundae", City: "Milkshake", State: "IL", Street: "Rocky Road",Building_Number: 216, icecream:["Cookies & Cream"] }),
-  new Store({_id: "Sundae_Fundae", City: "Cherry", State: "NH", Street: "Dairy Ave",Building_Number: 305, icecream:["Cookies & Cream"] }),
-  new Store({_id: "Sundae_Fundae", City: "Sprinkles", State: "PA", Street: "Strawberry Lane",Building_Number: 5, icecream:["Cookies & Cream"] })
+  new Store({_id: "Sundae_Funda", City: "Cherry", State: "NH", Street: "Dairy Ave",Building_Number: 305, icecream:["Cookies & Cream"] }),
+  new Store({_id: "Sundae_Funde", City: "Sprinkles", State: "PA", Street: "Strawberry Lane",Building_Number: 5, icecream:["Cookies & Cream"] })
 ];
 
 // Reset the database
