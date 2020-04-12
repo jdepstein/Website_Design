5a81739325db14cb6a1af4bac675844e616709d4
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 12:39:41 -0400

    };

--- Server/icecreams.js
+++ Server/icecreams.js
@@ -1,12 +1,9 @@
 const IceCream = require('../DB/IceCream_Sechma');
 
 module.exports.index = function(request, response, next) {
-  const order = request.query.sort || 'Name'; 
+  const order = request.query.sort || 'Name';
 
   Store.find().sort(order)
     .then(icecreams => response.render('icecreams/index', {icecreams: icecreams, order: order}))
     .catch(error => next(error));
-};
-
-
 };
