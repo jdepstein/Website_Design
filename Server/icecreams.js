5019b62a8f01280c867ad7559ee580fec6993f77
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 12:41:10 -0400

    yeah

--- Server/icecreams.js
+++ Server/icecreams.js
@@ -3,7 +3,7 @@ const IceCream = require('../DB/IceCream_Sechma');
 module.exports.index = function(request, response, next) {
   const order = request.query.sort || 'Name';
 
-  Store.find().sort(order)
+  IceCream.find().sort(order)
     .then(icecreams => response.render('icecreams/index', {icecreams: icecreams, order: order}))
     .catch(error => next(error));
 };
