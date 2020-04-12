cef84e909c696c349e54ada6362a530f84a77e1a
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 11:56:20 -0400

    redirect

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(request, response, next) {
 
 // Redirect from the home page
 app.get('/', function(request, response) {
-  response.redirect('/icecreams');
+  response.redirect('/stores');
 });
 
 // Route content requests
