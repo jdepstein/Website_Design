14d477b89568a7a36bbe1fe0b3febeb4827c0e34
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 11:42:22 -0400

    really dumb mistake wrong retrive val

--- router.js
+++ router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/stores', stores.index);
 router.get('/icecreams', icecreams.index);
-router.get('/icecreams/:Name', stores.retrieve);
+router.get('/icecreams/:Name', icecreams.retrieve);
 
 // Export the router
 module.exports = router;
