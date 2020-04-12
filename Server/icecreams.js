13a67f1e3f40024756773c8e38049088f76a06b5
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 12:33:02 -0400

    hope it works

--- Server/icecreams.js
+++ Server/icecreams.js
@@ -1,11 +1,11 @@
 const IceCream = require('../DB/IceCream_Sechma');
 
-// GET /sections?sort=
 module.exports.index = function(request, response, next) {
-  const order = request.query.sort || 'Name'; // Default to sort by course
+  const order = request.query.sort || 'Name';
 
   Section.find().sort(order)
     .then(icecreams => response.render('icecreams/index', {icecreams: icecreams, order: order}))
     .catch(error => next(error));
 
-  };
+    
+};
