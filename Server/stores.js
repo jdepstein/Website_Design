86613aaf5c2fa9fe0c4a3e2b1bdc1953671b72b1
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 11:04:44 -0400

    fixed

--- Server/stores.js
+++ Server/stores.js
@@ -4,7 +4,7 @@ const Store = require('../DB/Store_Schema');
 module.exports.index = function(request, response, next) {
   const order = request.query.sort || '_id'; // Default to sort by course
 
-  IceCream.find().sort(order)
+  Store.find().sort(order)
     .then(stores => response.render('stores/index', {stores: stores, order: order}))
     .catch(error => next(error));
 };
