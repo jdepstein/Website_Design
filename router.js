89ce7f7066d216e88171d7f0cb6ff4dc5b78230c
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-12 12:00:53 -0400

    id

--- router.js
+++ router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/stores', stores.index);
 router.get('/icecreams', icecreams.index);
-router.get('/icecreams/:Name', icecreams.retrieve);
+router.get('/icecreams/:id', icecreams.retrieve);
 
 // Export the router
 module.exports = router;
