134885658631c59d1082b8175edb73f2db15ccba
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-13 13:15:06 -0400

    custom css

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ connect();
 // Create the server
 const app = express();
 
+
 // Configure the views
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(express.static('public'))
 
 // Ignore icon requests
 app.get('/favicon.ico', function(request, response) {
