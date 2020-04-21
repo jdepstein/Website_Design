8779e54f7700a669d57dae02b03dfae0825742bd
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-04-20 21:55:23 -0500

    trying to get info page

---
+++ controllers/aboutus.js
@@ -0,0 +1,11 @@
+/*
+const Store = require('../models/Store_Schema');
+
+
+module.exports.index = function(request, response, next) {
+  const order = request.query.sort || '_id';
+
+  Store.find().sort(order)
+    .then(stores => response.render('stores/index', {stores: stores, order: order}))
+    .catch(error => next(error));
+};*/
---
+++ models/About_Schema.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+// Define the schema
+
+
+// Export the model
+//module.exports = mongoose.model('About');
