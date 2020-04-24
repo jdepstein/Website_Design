1a13c4727bab6859fef969246d137a2473ab841b
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-04-24 13:03:15 -0400

    maybe

--- models/About_Schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Define the schema
-const About = new mongoose.Schema({
-  description: String,
-});
-
-// Export the model
-module.exports = mongoose.model('About', About);
