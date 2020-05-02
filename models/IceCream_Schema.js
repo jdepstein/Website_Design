3fcb019c9d96beb58e262ca17cd7d54540e86231
Dakota Golde <dakotagolde@Dakotas-MacBook-Pro-2.local>
2020-05-02 10:50:34 -0500

    fixing error

--- models/IceCream_Schema.js
+++ models/IceCream_Schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const IceCream = new mongoose.Schema({
-  _id: {type: String, required: true, maxlength: 20, trim: true},
+  _id: {type: String, required: true, maxlength: 30, trim: true},
   flavors: [{type:String, required: true, maxlength: 25, trim: true}],
   fillings: [{type:String, maxlength: 25, trim: true}],
   description: {type:String, required: true, trim: true},
