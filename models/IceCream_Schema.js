159b95fa74f77f28a2cc50c69ce661d0b9ee5ef5
Joshua Epstein <jepstein@Joshuas-MacBook-Pro-2.local>
2020-05-03 14:58:11 -0400

    require

--- models/IceCream_Schema.js
+++ models/IceCream_Schema.js
@@ -4,8 +4,8 @@ const IceCream = new mongoose.Schema({
   _id: {type: String, required: true, maxlength: 30, trim: true},
   flavors: [{type:String, required: true, maxlength: 25, trim: true}],
   fillings: [{type:String, maxlength: 25, trim: true}],
-  description: {type:String, required: true, trim: true},
-  image: {type:String, required: true, trim: true}
+  description: {type:String, trim: true},
+  image: {type:String,  trim: true}
 });
 
 
